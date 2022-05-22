678da4d75092bd9e18c367b336bbf6f7bfe6bc4b
johngamecoder <johngamecoder@gmail.com>
2022-05-22 21:17:51 +0900

    This is a combination of 2 commits.
    Device Initialize 장치 초기화

--- Game/Client/Client.cpp
+++ Game/Client/Client.cpp
@@ -8,6 +8,8 @@
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
+WindowInfo GWindowInfo;
+
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
@@ -43,8 +45,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
+	GWindowInfo.width = 800;
+	GWindowInfo.height = 600;
+	GWindowInfo.windowed = true;
+
 	unique_ptr<Game> game = make_unique<Game>();
-	game->Init();
+	game->Init(GWindowInfo);
 
     // 기본 메시지 루프입니다:
     while (true)
@@ -121,6 +127,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
+
+   GWindowInfo.hwnd = hWnd;
 
    return TRUE;
 }
