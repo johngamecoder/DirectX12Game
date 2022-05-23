d6b3711832fa55a5600b2e0f82461b2454d39629
johngamecoder <johngamecoder@gmail.com>
2022-05-24 07:37:18 +0900

    Draw Triangle 삼각형 띄우기

--- Game/Engine/Engine.h
+++ Game/Engine/Engine.h
@@ -1,9 +1,22 @@
 #pragma once
+
+#include "Device.h"
+#include "CommandQueue.h"
+#include "SwapChain.h"
+#include "RootSignature.h"
+#include "Mesh.h"
+#include "Shader.h"
+
 class Engine
 {
 public:
 	void Init(const WindowInfo& info);
 	void Render();
+
+	shared_ptr<Device> GetDevice() { return _device; }
+	shared_ptr<CommandQueue> GetCmdQueue() { return _cmdQueue; }
+	shared_ptr<SwapChain> GetSwapChain() { return _swapChain; }
+	shared_ptr<RootSignature> GetRootSignature() { return _rootSignature; }
 
 	void RenderBegin();
 	void RenderEnd();
@@ -16,9 +29,9 @@ private:
 	D3D12_VIEWPORT	_viewport = {};
 	D3D12_RECT		_scissorRect = {};
 
-	shared_ptr<class Device> _device;
-	shared_ptr<class CommandQueue> _cmdQueue;
-	shared_ptr<class SwapChain> _swapChain;
-	shared_ptr<class DescriptorHeap> _descHeap;
+	shared_ptr<Device> _device;
+	shared_ptr<CommandQueue> _cmdQueue;
+	shared_ptr<SwapChain> _swapChain;
+	shared_ptr<RootSignature> _rootSignature;
 };
 
--- Game/Engine/EnginePch.h
+++ Game/Engine/EnginePch.h
@@ -57,5 +57,15 @@ struct WindowInfo
 	bool  windowed; //창모드 or 전체화면
 };
 
+struct Vertex
+{
+	Vec3 pos;
+	Vec4 color;
+};
+
+#define DEVICE			GEngine->GetDevice()->GetDevice()
+#define CMD_LIST		GEngine->GetCmdQueue()->GetCmdList()
+#define ROOT_SIGNATURE	GEngine->GetRootSignature()->GetSignature()
+
 extern unique_ptr<class Engine> GEngine;
 
