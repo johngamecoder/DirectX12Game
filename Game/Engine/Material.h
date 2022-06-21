cef92acb3ed9453bbbc3fcedf2253ff9d6e5e1c8
johngamecoder <johngamecoder@gmail.com>
2022-06-21 12:42:43 +0900

    Instancing

--- Game/Engine/Material.h
+++ Game/Engine/Material.h
@@ -52,6 +52,8 @@ public:
 	void PushComputeData();
 	void Dispatch(uint32 x, uint32 y, uint32 z);
 
+	shared_ptr<Material> Clone();
+
 private:
 	shared_ptr<Shader>	_shader;
 	MaterialParams		_params;
